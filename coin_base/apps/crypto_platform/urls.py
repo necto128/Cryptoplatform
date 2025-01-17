5583bf0704e62e1029da91f47e457975db46e789
necto128 <nester1012000@mail.ru>
2025-01-18 00:37:36 +0300

    add functionality for get history directive

--- coin_base/apps/crypto_platform/urls.py
+++ coin_base/apps/crypto_platform/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 from apps.crypto_platform.view.order import CreateStaticBuyOrderView, CreateStaticSellOrderView
 from apps.crypto_platform.view.subscription import SubscriptionDirectiveView
 from apps.crypto_platform.view.recommendations import RecommendationsView
+from apps.crypto_platform.view.coin import HistoryCryptView
 
 urlpatterns = [
     path("create-order-buy/", CreateStaticBuyOrderView.as_view(), name="order-static"),
     path("create-order-sell/", CreateStaticSellOrderView.as_view(), name="order-static"),
     path("subscription/", SubscriptionDirectiveView.as_view(), name="subscription"),
     path("recommendations/", RecommendationsView.as_view(), name="recommendations"),
+    path("history-crypt/<int:crypto_id>/", HistoryCryptView.as_view(), name="crypt-history"),
 ]
