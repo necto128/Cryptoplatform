efbc8b66db4b06c323bb8f61b9dd9517dd7ff8d7
necto128 <nester1012000@mail.ru>
2025-01-20 23:52:53 +0300

    added analytic functionality

--- coin_base/apps/crypto_platform/urls.py
+++ coin_base/apps/crypto_platform/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 
-from apps.crypto_platform.view.admin import AdminGetCryptView, AdminGetOrderCryptView, AdminUserManagementView
+from apps.crypto_platform.view.admin import (
+    AdminGetCryptView,
+    AdminGetOrderCryptView,
+    AdminUserManagementView
+)
+from apps.crypto_platform.view.analytic import (
+    AnalyticGetOrderCryptView,
+    AnalyticGetUserView,
+    AnalyticGetTransactionsView
+)
 from apps.crypto_platform.view.coin import HistoryCryptView
 from apps.crypto_platform.view.order import CreateStaticBuyOrderView, CreateStaticSellOrderView
 from apps.crypto_platform.view.recommendations import RecommendationsView
@@ -12,6 +21,11 @@ urlpatterns = [
     path("subscription/", SubscriptionDirectiveView.as_view(), name="subscription"),
     path("recommendations/", RecommendationsView.as_view(), name="recommendations"),
     path("history-crypt/<int:crypto_id>/", HistoryCryptView.as_view(), name="crypt-history"),
+    # analytic
+    path("analytic/order/", AnalyticGetOrderCryptView.as_view(), name="analytic-order"),
+    path("analytic/user/", AnalyticGetUserView.as_view(), name="analytic-user"),
+    path("analytic/transactions/<int:user_id>/", AnalyticGetTransactionsView.as_view(), name="analytic-transactions"),
+    # administrator
     path("admins/crypts/", AdminGetCryptView.as_view(), name="admin-crypt"),
     path("admins/order/", AdminGetOrderCryptView.as_view(), name="admin-order"),
     path("admins/user/", AdminUserManagementView.as_view(), name="admin-user"),
